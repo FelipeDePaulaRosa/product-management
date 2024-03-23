9b7beb23dd49ae9db3c6cfb1fbbd199319b37bcf
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2024-03-23 16:50:49 -0300

    Added properties on create product request and created validator

--- Application/Products/CreateProduct/CreateProductRequest.cs
+++ Application/Products/CreateProduct/CreateProductRequest.cs
@@ -2,8 +2,13 @@
 
 namespace Application.Products.CreateProduct
 {
-    public class CreateProductRequest: IRequest<CreateProductResponse>
+    public class CreateProductRequest : IRequest<CreateProductResponse>
     {
-        
+        public string Description { get; set; }
+        public string ManufactureDate { get; set; }
+        public string DueDate { get; set; }
+        public string SupplierCode { get; set; }
+        public string SupplierDescription { get; set; }
+        public string SupplierCnpj { get; set; }
     }
 }
---
+++ Application/Products/CreateProduct/CreateProductValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace Application.Products.CreateProduct
+{
+    public class CreateProductValidator: AbstractValidator<CreateProductRequest>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Description)
+                .NotEmpty()
+                .WithMessage("O campo Descrição é obrigatório");
+        }
+    }
+}
