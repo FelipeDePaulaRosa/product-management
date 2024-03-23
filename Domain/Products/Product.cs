1ae70fc2662a66c176236c6ad708d5d9b5facf66
FelipeDePaulaRosa <felipedepaularosa11@gmail.com>
2024-03-23 17:30:11 -0300

    Created entity product with value object supplier

---
+++ Domain/Products/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Domain.Utils.Entities;
+
+namespace Domain.Products
+{
+    public class Product : AggregateRoot<Guid>
+    {
+        public string Code { get; }
+        public string Description { get; private set; }
+        public bool IsActive { get; private set; }
+        public DateTime ManufactureDate { get; private set; }
+        public DateTime DueDate { get; private set; }
+        public Supplier Supplier { get; private set; }
+
+        private Product() { }
+        
+        public Product(
+            string description,
+            bool isActive,
+            DateTime manufactureDate,
+            DateTime dueDate,
+            string supplierCode,
+            string supplierDescription,
+            string supplierCnpj)
+        {
+            Description = description;
+            IsActive = isActive;
+            ManufactureDate = manufactureDate;
+            DueDate = dueDate;
+            Supplier = new Supplier(supplierCode, supplierDescription, supplierCnpj);
+        }
+    }
+}
---
+++ Domain/Products/Supplier.cs
@@ -0,0 +1,16 @@
+﻿namespace Domain.Products
+{
+    public class Supplier
+    {
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public string Cnpj { get; set; }
+        
+        public Supplier(string code, string description, string cnpj)
+        {
+            Code = code;
+            Description = description;
+            Cnpj = cnpj;
+        }
+    }
+}
