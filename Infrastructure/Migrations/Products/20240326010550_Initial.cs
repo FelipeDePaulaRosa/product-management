9e903cd19cc0c3c74c38ae46a0c8e0a9702a64cd
felipe.rosa <felipe.rosa@tinnova.com.br>
2024-03-25 22:31:57 -0300

    Added migrations for product model

---
+++ Infrastructure/Migrations/Products/20240326010550_Initial.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infrastructure.Migrations.Products
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false, defaultValueSql: "NEXT VALUE FOR product_management.dbo.ProductSequence"),
+                    Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false),
+                    ManufactureDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Supplier_Code = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: true),
+                    Supplier_Description = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true),
+                    Supplier_Cnpj = table.Column<string>(type: "nvarchar(14)", maxLength: 14, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
